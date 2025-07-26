9a68d2f5616e6e91fbaa923fcbad7b52c5b38a15
mafiatamilan <mafiatamilan@yahoo.com>
2025-07-26 10:41:31 +0530

    minor fixes

--- core/settings.py
+++ core/settings.py
@@ -80,12 +80,16 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URL")
-    )
-}
+ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
 
+if ENVIRONMENT == "production":
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.config()
+    }
 
 
 
