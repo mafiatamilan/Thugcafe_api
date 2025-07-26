c6120df0702a31ac0fbc8eb049d5c69de4982265
mafiatamilan <mafiatamilan@yahoo.com>
2025-07-26 08:27:21 +0530

    minor fixed

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Wait for PostgreSQL to be available
+echo "Waiting for PostgreSQL..."
+while ! nc -z db 5432; do
+  sleep 1
+done
+echo "PostgreSQL is up."
+
 echo "Applying migrations..."
 python manage.py migrate --noinput
 
@@ -7,4 +14,4 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "Starting server..."
-gunicorn core.wsgi:application --bind 0.0.0.0:8000
+exec gunicorn core.wsgi:application --bind 0.0.0.0:8000
