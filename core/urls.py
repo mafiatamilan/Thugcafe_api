6fe26f08684a9a9b4f56135288e276556b3da87f
mafiatamilan <mafiatamilan@yahoo.com>
2025-07-26 09:55:58 +0530

    swagger

--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
-
-# Swagger imports
-from rest_framework import permissions
+from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
         title="Thugcafe API",
         default_version='v1',
-        description="API documentation for Thugcafe",
+        description="API docs for Thugcafe",
         contact=openapi.Contact(email="samplepos@gmail.com"),
     ),
     public=True,
@@ -21,8 +19,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 
-    # Swagger & ReDoc endpoints
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    # Swagger UI:
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # Redoc:
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
