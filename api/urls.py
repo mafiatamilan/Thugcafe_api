3cefd9bbefd2b5ca7b7040947e4d48269731bbbe
mafiatamilan <mafiatamilan@yahoo.com>
2025-07-27 09:56:11 +0530

    minor fixes

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('POS_APP.urls')),  # <- Include accounts app URLs here
+    path('api/', include('POS_APP.urls')),  # <- Include accounts app URLs here
 ]
